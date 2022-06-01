96f75e0c214f8f277bf402f4cfda789659b7caa4
jattonwheels <aayush0032.be21@chitkara.edu.in>
2022-06-01 20:26:07 +0530

    salaryy check

---
+++ salary.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Employee
+{
+    int id;
+    string name;
+    int salary;
+};
+
+int main()
+{
+
+    Employee e1, e2;
+    cin >> e1.id >> e1.name >> e1.salary;
+    cin >> e2.id >> e2.name >> e2.salary;
+
+
+
+
+
+
+
+
+
+
+
+     return 0;
+}
